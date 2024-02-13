75463f8b02e57355706c460a58e8a658df4da7ff
lumikil <kilpelainenlumi@gmail.com>
2024-02-13 11:55:43 +0200

    Update pipex.h with necessary header files and include libft.h

--- pipex.h
+++ pipex.h
@@ -6,11 +6,21 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/13 11:50:42 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/13 11:55:37 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+# include <fcntl.h> //open
+//close, read, write, access, dup, dup2, execve, fork, pipe, unlink
+# include <unistd.h>  
+# include <stdlib.h> //malloc, free, exit
+# include <stdio.h> // perror
+# include <string.h> //strerror
+# include <sys/wait.h> // wait, waitpid
+# include "../lib/libft/include/libft.h"
+
 
 #endif
