497de045f80570da7c8444dc3c46aef8f5c1e9e6
lumikil <kilpelainenlumi@gmail.com>
2024-02-13 12:52:05 +0200

    Add find_path function to path.c & update pipex.h with enum

--- path.c
+++ path.c
@@ -6,8 +6,18 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 12:08:08 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/13 12:08:21 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/13 12:16:45 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static char *find_path(t_pipex *p)
+{
+    int i;
+
+    i = 0;
+    while (p->envp[i] && !ft_strncmp(p->envp[i], "PATH=", 5))
+		i++;
+    
+}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/13 12:06:43 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/13 12:47:38 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,30 @@
 # include <sys/wait.h> // wait, waitpid
 # include "../lib/libft/include/libft.h"
 
+typedef enum e_error
+{
+	NO_ERROR,
+	ERR_FORK,
+	ERR_PIPE,
+	ERR_DUP2,
+	ERR_EXECVE,
+	ERR_MALLOC,
+	ERR_OPEN,
+	ERR_CLOSE,
+	ERR_READ,
+	ERR_WRITE
+}			t_error;
+
 typedef struct s_pipex
 {
-    int     argc;
-    char    **argv;
-    char    **envp;
-    pid_t   *pids;
-    int     infile;
-    int     outfile;
-    char    **paths;
-    char    **cmd;
-}              t_pipex;
+	int		argc;
+	char	**argv;
+	char	**envp;
+	pid_t	*pids;
+	int		infile;
+	int		outfile;
+	char	**paths;
+	char	**cmd;
+}				t_pipex;
 
 #endif
