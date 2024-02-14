86128729f6601cafbf4d4bf049c743b0243e9064
lumikil <kilpelainenlumi@gmail.com>
2024-02-14 12:34:39 +0200

    Update handle_syntax function to check for correct number of arguments

--- pipex.c
+++ pipex.c
@@ -6,10 +6,18 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:47 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/14 11:46:53 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/14 12:34:31 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static int  handle_syntax(int argc, char **argv)
+{
+    if (argc != 5)
+    {
+        return (ERR_SYNTAX);
+    }
+}
 
 int main(int argc, char **argv, char **envp);
