9bc885109428b10c13349daac1b072b6cd669ffa
lumikil <kilpelainenlumi@gmail.com>
2024-02-14 10:48:22 +0200

    Refactor error handling in error.c

--- error.c
+++ error.c
@@ -6,15 +6,35 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 10:37:58 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/14 10:41:21 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/14 10:47:10 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-t_error check_memory(void *ptr)
+void	error(t_error e)
 {
-    if (!ptr)
-        return (ERR_MEM);
-    return (ERR_NONE);
+    if (e == ERR_FORK)
+        perror("fork failed");
+    else if (e == ERR_PIPE)
+        perror("pipe failed");
+    else if (e == ERR_DUP2)
+        perror("dup2 failed");
+    else if (e == ERR_EXECVE)
+        perror("execve failed");
+    else if (e == ERR_MALLOC)
+        perror("malloc failed");
+    else if (e == ERR_OPEN)
+        perror("open failed");
+    else if (e == ERR_CLOSE)
+        perror("close failed");
+    else if (e == ERR_READ)
+        perror("read failed");
+    else if (e == ERR_WRITE)
+        perror("write failed");
+    else if (e == ERR_MEM)
+        perror("Memory allocation failed");
+    else
+        perror("Unknown error");
+    exit(1);
 }
