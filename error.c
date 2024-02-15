298a8ec234072e101086af9c38d4ac5ea8f68641
lumikil <kilpelainenlumi@gmail.com>
2024-02-15 11:18:04 +0200

    Refactor error handling and cleanup code

--- error.c
+++ error.c
@@ -6,14 +6,39 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 10:37:58 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/14 12:39:03 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/15 11:17:51 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	error(int e)
+void	free_paths(char **paths)
 {
+	int	i;
+
+	i = 0;
+	while (paths[i])
+	{
+		free(paths[i]);
+		i++;
+	}
+	free(paths);
+}
+
+void cleanup(t_pipex *p)
+{
+	if (p->full_path)
+		free(p->full_path);
+	if (p->slash_cmd)
+		free(p->slash_cmd);
+	if (p->paths)
+		free_paths(p->paths);
+	// Add more cleanup code as needed...
+}
+
+void	error(t_pipex *p, int e)
+{
+	cleanup(p);
 	if (e == ERR_SYNTAX)
 		perror("Syntax: ./pipex infile cmd1 cmd2 outfile\n");
 	else if (e == ERR_FORK)
