cb7916f8a90225502bbe33420bcbf1a601a9043f
lumikil <kilpelainenlumi@gmail.com>
2024-02-16 13:59:15 +0200

    Refactor tokenize functions

--- spit.c
+++ spit.c
@@ -6,13 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:30:19 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/16 13:57:28 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/16 13:58:53 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void init_tokenize(t_tokenize *t)
+void	init_tokenize(t_tokenize *t)
 {
 	t->count = 0;
 	t->in_quotes = 0;
@@ -20,48 +20,48 @@ void init_tokenize(t_tokenize *t)
 	t->arg = NULL;
 }
 
-void    toggle_quotes(t_tokenize *t, char c)
+void	toggle_quotes(t_tokenize *t, char c)
 {
-    if (c == '"' || c == '\'')
-    t->in_quotes = !t->in_quotes;
+	if (c == '"' || c == '\'')
+	t->in_quotes = !t->in_quotes;
 }
 
-void    add_word(t_tokenize *t, char *start, char *end)
+void	add_word(t_tokenize *t, char *start, char *end)
 {
-    t->args = resize_array(t->args, t->count, t->count + 1);
-    t->args[t->count++] = ft_substr(start, 0, end - start);
+	t->args = resize_array(t->args, t->count, t->count + 1);
+	t->args[t->count++] = ft_substr(start, 0, end - start);
 }
 
-void    split_on_space(t_tokenize *t, char *cmd)
+void	split_on_space(t_tokenize *t, char *cmd)
 {
-    char    *end;
-    char    *start;
+	char	*end;
+	char	*start;
 
-    start = cmd;
-    end = cmd;
-    while (*end)
-    {
-        toggle_quotes(t, *end);
-        if (*end == ' ' && !t->in_quotes)
-        {
-            add_word(t, start, end);
-            start = end + 1;
-        }
-        end++;
-    }
-    if (end != start)
-        add_word(t, start, end);
+	start = cmd;
+	end = cmd;
+	while (*end)
+	{
+		toggle_quotes(t, *end);
+		if (*end == ' ' && !t->in_quotes)
+		{
+			add_word(t, start, end);
+			start = end + 1;
+		}
+		end++;
+	}
+	if (end != start)
+		add_word(t, start, end);
 }
 
 void    split_command(t_tokenize *t, char *cmd)
 {
-    char    *start;
-    char    *end;
+	char	*start;
+	char	*end;
 
-    start = cmd;
-    end = cmd;
+	start = cmd;
+	end = cmd;
 
-    split_on_space(t, cmd);
-    t->args = resize_array(t->args, t->count, t->count + 1);
-    t->args[t->count] = NULL;
+	split_on_space(t, cmd);
+	t->args = resize_array(t->args, t->count, t->count + 1);
+	t->args[t->count] = NULL;
 }
