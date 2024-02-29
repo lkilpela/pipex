c1346677abaca5636484d6e739e1eada501d6985
lumikil <kilpelainenlumi@gmail.com>
2024-02-29 14:08:03 +0200

    Update execute_child function signature

--- child.c
+++ child.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:40:37 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/29 14:06:43 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/29 14:07:57 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	setup_second_command(t_pipex *p)
 		error(ERR_CLOSE);
 }
 
-int	execute_child( t_pipex *p, t_tokenize *t, char *cmd)
+void	execute_child( t_pipex *p, t_tokenize *t, char *cmd)
 {
 	int	status;
 
