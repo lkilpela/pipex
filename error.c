1fbc8b5f343217f558175b065e332e2c4dacaddc
lumikil <kilpelainenlumi@gmail.com>
2024-02-29 09:25:58 +0200

    Update error handling and remove unused code

---
+++ error.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 09:25:19 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/02/29 09:25:50 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+const char	*get_error_messages(int e)
+{
+	static const char	*error_messages[] = {
+		"No error",
+		"Syntax: ./pipex infile cmd1 cmd2 outfile",
+		"Memory allocation failed",
+		"fork failed",
+		"pipe failed",
+		"dup2 failed",
+		"execve failed",
+		"malloc failed",
+		"open failed",
+		"close failed",
+		"read failed",
+		"write failed",
+		"waitpid failed",
+		"Command not found"
+	};
+
+	return (error_messages[e]);
+}
+
+void	error(int e)
+{
+
+	if (e >= 0 || e < LAST_ERROR)
+		perror(get_error_messages(e));
+	else
+		perror ("Unknown error");
+	exit(EXIT_FAILURE);
+}
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 10:37:58 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/29 09:21:02 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/29 09:25:47 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,38 +48,6 @@ static void	cleanup(t_pipex *p)
 		free(p->parent_path);
 	if (p->paths)
 		free_paths(p->paths);
-}
-
-const char	*get_error_messages(int e)
-{
-	static const char	*error_messages[] = {
-		"No error",
-		"Syntax: ./pipex infile cmd1 cmd2 outfile",
-		"Memory allocation failed",
-		"fork failed",
-		"pipe failed",
-		"dup2 failed",
-		"execve failed",
-		"malloc failed",
-		"open failed",
-		"close failed",
-		"read failed",
-		"write failed",
-		"waitpid failed",
-		"Command not found"
-	};
-
-	return (error_messages[e]);
-}
-
-void	error(int e)
-{
-
-	if (e >= 0 || e < LAST_ERROR)
-		perror(get_error_messages(e));
-	else
-		perror ("Unknown error");
-	exit(EXIT_FAILURE);
 }
 
 char	**resize_array(char **old_array, int old_count, int new_count)
