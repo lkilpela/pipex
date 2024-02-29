546f73852c811a2a2525d738ef9533e7f6cf2ac6
lumikil <kilpelainenlumi@gmail.com>
2024-02-29 15:12:55 +0200

    Add parent_process function to execute commands

--- parent.c
+++ parent.c
@@ -6,9 +6,30 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/28 21:18:30 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:12:53 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int parent_process(t_pipex *p, t_tokenize *t, char *cmd)
+{
+	int	status;
+	int	exit_status;
+
+	status = execute_first_command(p, t, cmd);
+	if (status != 0)
+		return (status);
+	status = execute_second_command(p, t, cmd);
+	if (status != 0)
+		return (status);
+	if (t->pid > 0)
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status)) 
+		{
+			exit_status = WEXITSTATUS(status);
+		}
+	}
+	return (0);
+}
