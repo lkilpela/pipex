067cbf4e225f1a1484887046cca0ff26a19ff0e7
lumikil <kilpelainenlumi@gmail.com>
2024-02-29 15:23:55 +0200

    Update parent_process function to return exit status

--- parent.c
+++ parent.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/29 15:12:53 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:16:16 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,20 @@ int parent_process(t_pipex *p, t_tokenize *t, char *cmd)
 	int	status;
 	int	exit_status;
 
+	exit_status = 0;
 	status = execute_first_command(p, t, cmd);
 	if (status != 0)
 		return (status);
 	status = execute_second_command(p, t, cmd);
 	if (status != 0)
 		return (status);
-	if (t->pid > 0)
+	if (p->pid > 0)
 	{
-		waitpid(pid, &status, 0);
+		waitpid(p->pid, &status, 0);
 		if (WIFEXITED(status)) 
 		{
 			exit_status = WEXITSTATUS(status);
 		}
 	}
-	return (0);
+	return (exit_status);
 }
