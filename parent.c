1ef592f72a822a3d5b1e6932923088e60237bd11
lumikil <kilpelainenlumi@gmail.com>
2024-03-01 09:07:55 +0200

    Fix wait_children function and add handle_signal function

--- parent.c
+++ parent.c
@@ -6,18 +6,27 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/01 08:54:01 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/01 09:07:26 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int handle_signal(int status)
+{
+	int	signal_number;
+
+	if (signal_number == WTERMSIG(status))
+}
+
 int wait_children(t_pipex *p, t_tokenize *t, char *cmd)
 {
-	int	i;
-	int	status;
-	int	exit_status;
+	pid_t	pid;
+	int		i;
+	int		status;
+	int		exit_status;
 
+	i = 0;
 	exit_status = 0;
 	status = execute_first_command(p, t, cmd);
 	if (status != 0)
@@ -25,12 +34,16 @@ int wait_children(t_pipex *p, t_tokenize *t, char *cmd)
 	status = execute_second_command(p, t, cmd);
 	if (status != 0)
 		return (status);
-	while (p->pids[i] < 2)
+	while (i < 2)
 	{
-		waitpid(p->pid[i], &status, 0);
-		if (WIFEXITED(status)) 
-		{
+		pid = waitpid(p->pid[i], &status, 0);
+		if (pid == -1)
+			error(ERR_WAITPID);
+		else if (WIFEXITED(status)) 
 			exit_status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+		{
+			
 		}
 		i++;
 	}
