ba1e4bc636fd8ac2570eb2b4e35ea018a9640c46
lumikil <kilpelainenlumi@gmail.com>
2024-03-01 09:33:53 +0200

    fix norm

--- wait.c
+++ wait.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parent.c                                           :+:      :+:    :+:   */
+/*   wait.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/01 09:30:03 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/01 09:33:46 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int wait_children(t_pipex *p, t_tokenize *t, char *cmd)
+int	wait_children(t_pipex *p, t_tokenize *t, char *cmd)
 {
 	pid_t	pid;
 	int		i;
@@ -30,7 +30,7 @@ int wait_children(t_pipex *p, t_tokenize *t, char *cmd)
 		pid = waitpid(p->pid[i], &status, 0);
 		if (pid == -1)
 			error(ERR_WAITPID);
-		else if (WIFEXITED(status)) 
+		else if (WIFEXITED(status))
 			ft_printf("Child with PID %d exited with status %d.\n", pid,
 				WEXITSTATUS(status));
 		else if (WIFSIGNALED(status))
