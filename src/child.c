e8c8f1e3aa68c9d4529615acf7d1e2537f673fe6
lumikil <kilpelainenlumi@gmail.com>
2024-03-06 18:38:41 +0200

    Fix file descriptor handling and error reporting

--- src/child.c
+++ src/child.c
@@ -6,30 +6,57 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:40:37 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/01 14:05:44 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/06 14:10:26 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <sys/errno.h>
 
 //1st child: any reads from STDIN: read from the input file, 
 //any writes to STDOUT: write to the pipe.
 int	setup_first_command(t_pipex *p)
 {
+	int ret;
+	printf("In setup_first_command, p->argv[1] = %s\n", p->argv[1]);
+	
 	p->infilefd = open(p->argv[1], O_RDONLY);
+	printf("open returned %d\n", p->infilefd);
 	if (p->infilefd == -1)
 		error(ERR_OPEN);
-	if (dup2(p->infilefd, STDIN_FILENO) == -1)
+
+	printf("Before first dup2\n");
+	ret = dup2(p->infilefd, STDIN_FILENO);
+	printf("First dup2 returned %d, errno = %d\n", ret, errno);
+	printf("p->infilefd = %d, STDIN_FILENO = %d\n", p->infilefd, STDIN_FILENO);
+	if (ret == -1)
 	{
-		close(p->infilefd);
-		error(ERR_DUP2);
+    	printf("dup2 failed, errno = %d\n", errno);
+	} else {
+    	printf("dup2 succeeded\n");
 	}
-	if (dup2(p->pipefd[1], STDOUT_FILENO) == -1)
+	//{
+		//printf("First dup2 failed\n");
+		//close(p->infilefd);
+		//error(ERR_DUP2);
+	//}
+	printf("p->pipefd[1] = %d\n", p->pipefd[1]);
+	printf("After first dup2\n");
+	
+	printf("Before second dup2\n");
+
+	ret = dup2(p->pipefd[1], STDOUT_FILENO);
+	printf("Second dup2 returned %d, errno = %d\n", ret, errno);
+	printf("p->pipefd[1] = %d, STDOUT_FILENO = %d\n", p->pipefd[1], STDOUT_FILENO);
+	printf("After second dup2\n");
+	if (ret == -1)
 	{
+		printf("Second dup2 failed\n");
 		close(p->pipefd[1]);
 		close(p->infilefd);
 		error(ERR_DUP2);
 	}
+	printf("Exiting setup_first_command\n");
 	return (0);
 }
 
@@ -85,16 +112,30 @@ int	execute_command( t_pipex *p, t_tokenize *t, char *cmd)
 int	execute_first_command(t_pipex *p, t_tokenize *t)
 {
 	int	status;
-
+	
+	printf("Before fork, PID = %d\n", getpid());
 	p->pids[0] = fork();
+	printf("After fork, PID = %d, fork returned %d\n", getpid(), p->pids[0]);
 	if (p->pids[0] == -1)
 		error(ERR_FORK);
-	if (p->pids[0] == 0)
+	else if (p->pids[0] == 0)
 	{
+		printf("In child process, PID = %d\n", getpid());
+		printf("Calling setup_first_command\n");
 		status = setup_first_command(p);
+		printf("setup_first_command returned %d\n", status);
 		if (status != 0)
+		{
+			fprintf(stderr, "setup_first_command failed with status %d\n", status);
 			exit(status);// Exit the child process with the error status
+		}
+		printf("Executing command: %s\n", p->argv[2]);
 		status = execute_command(p, t, p->argv[2]);
+		printf("execute_command returned %d\n", status);
+		if (status != 0)
+		{
+			fprintf(stderr, "execute_command failed with status %d\n", status);
+		}
 		exit(status);
 	}
 	close(p->infilefd);
--- src/pipex.c
+++ src/pipex.c
@@ -6,11 +6,12 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:47 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/01 13:32:05 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:32:20 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <signal.h>
 
 static void	handle_syntax(t_pipex *p)
 {
@@ -27,6 +28,11 @@ void	setup_pipe(t_pipex *p)
 	if (pipe(p->pipefd) == -1)
 		error(ERR_PIPE);
 }
+void handle_sigterm(int sig) 
+{
+    printf("Received termination signal %d\n", sig);
+    exit(1);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -34,6 +40,11 @@ int	main(int argc, char **argv, char **envp)
 	int			status;
 	t_tokenize	t;
 
+	printf("argv[1] = %s\n", argv[1]);
+	signal(SIGTERM, handle_sigterm);
+	signal(SIGINT, handle_sigterm);
+	signal(SIGQUIT, handle_sigterm);
+	signal(SIGSEGV, handle_sigterm);
 	init_pipex(argc, argv, envp, &p);
 	handle_syntax(&p);
 	setup_pipe(&p);
