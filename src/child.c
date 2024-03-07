70219b146ba9415dbc030b38657258df24b443f2
lumikil <kilpelainenlumi@gmail.com>
2024-03-07 15:33:36 +0200

    Fix error handling in execute_command function

--- src/child.c
+++ src/child.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:40:37 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/07 15:33:05 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/07 15:33:31 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,12 +63,12 @@ static int	execute_command( t_pipex *p, t_tokenize *t, char *cmd)
 {
 	p->child_cmd = split_command(t, cmd);
 	if (p->child_cmd == NULL || p->child_cmd[0] == NULL)
-		error(ERR_CMD_NOT_FOUND, &p);
+		error(ERR_CMD_NOT_FOUND, p);
 	p->child_path = find_command(p, p->child_cmd[0]);
 	if (!p->child_path)
 	{
 		free_paths(p->child_cmd);
-		error(ERR_CMD_NOT_FOUND, &p);
+		error(ERR_CMD_NOT_FOUND, p);
 	}
 	if (execve(p->child_path, p->child_cmd, p->envp) == -1)
 	{
