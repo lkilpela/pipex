96a680f01084c23d3fd39265b852ae692473424f
lumikil <kilpelainenlumi@gmail.com>
2024-03-07 10:07:43 +0200

    Update wait_children function to use ft_printf instead of printf

--- src/wait.c
+++ src/wait.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/01 13:38:08 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/07 10:07:39 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,12 @@ int	wait_children(t_pipex *p, t_tokenize *t)
 	while (i < 2)
 	{
 		pid = waitpid(p->pids[i], &status, 0);
-		printf("waitpid returned status: %d\n", status);
 		if (pid == -1)
 			error(ERR_WAITPID);
 		else if (WIFEXITED(status))
-			printf("Child with PID %d exited with status %d.\n", pid, WEXITSTATUS(status));
+			ft_printf("Child with PID %d exited with status %d.\n", pid, WEXITSTATUS(status));
 		else if (WIFSIGNALED(status))
-			printf("Child with PID %d was terminated by signal %d.\n", pid, WTERMSIG(status));
+			ft_printf("Child with PID %d was terminated by signal %d.\n", pid, WTERMSIG(status));
 		i++;
 	}
 	return (0);
