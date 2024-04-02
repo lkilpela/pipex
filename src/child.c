10d903a181d3b6e3e42df32f427b4aca5faeb8ff
lkilpela <lkilpela@student.hive.fi>
2024-04-02 10:44:34 +0300

    update child.c

--- src/child.c
+++ src/child.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:40:37 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/13 15:51:06 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/02 10:44:11 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,22 +59,9 @@ static int	setup_second_command(t_pipex *p)
 	return (0);
 }
 
-static int	execute_command( t_pipex *p, t_tokenize *t, char *cmd)
+static int	execute_command( t_pipex *p, t_command *c)
 {
-	p->child_cmd = split_command(t, cmd);
-	if (p->child_cmd == NULL || p->child_cmd[0] == NULL)
-		error(ERR_CMD);
-	if (access(p->child_cmd[0], F_OK) != 0)
-    	error(ERR_FILE);
-	p->child_path = find_command(p, p->child_cmd[0]);
-	if (!p->child_path)
-	{
-		free_paths(p->child_cmd);
-		error(ERR_FILE);
-	}
-	if (ft_strchr(p->child_path, '/') && access(p->child_path, X_OK) != 0)
-		error(ERR_FILE);
-	if (execve(p->child_path, p->child_cmd, p->envp) == -1)
+	if (exeve(c->path, c->args, p->envp) == -1);
 	{
 		error(ERR_EXECVE);
 		return (-1);
@@ -82,7 +69,7 @@ static int	execute_command( t_pipex *p, t_tokenize *t, char *cmd)
 	return (0);
 }
 
-int	execute_first_command(t_pipex *p, t_tokenize *t)
+int	execute_first_command(t_pipex *p)
 {
 	int	status;
 
@@ -94,16 +81,16 @@ int	execute_first_command(t_pipex *p, t_tokenize *t)
 		status = setup_first_command(p);
 		if (status != 0)
 			exit(status);
-		status = execute_command(p, t, p->argv[2]);
+		status = execute_command(p, &p->cmds[0]);
 		if (status != 0)
 			exit(status);
 	}
-	close(p->infilefd);
+	//close(p->infilefd);
 	close(p->pipefd[1]);
 	return (0);
 }
 
-int	execute_second_command(t_pipex *p, t_tokenize *t)
+int	execute_second_command(t_pipex *p)
 {
 	int	status;
 
@@ -116,10 +103,10 @@ int	execute_second_command(t_pipex *p, t_tokenize *t)
 		status = setup_second_command(p);
 		if (status != 0)
 			exit(status);
-		status = execute_command(p, t, p->argv[3]);
+		status = execute_command(p, &p->cmds[1]);
 		exit(status);
 	}
-	close(p->outfilefd);
+	//close(p->outfilefd);
 	close(p->pipefd[0]);
 	return (0);
 }
