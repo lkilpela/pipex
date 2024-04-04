38b414d36e4dadd7f731e67be639aee062289d00
lumikil <kilpelainenlumi@gmail.com>
2024-04-04 11:00:36 +0300

    Update error handling function

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:51 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/04 10:02:41 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/04 11:00:29 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,6 +106,7 @@ int		wait_children(t_pipex *p);
 
 // error handler
 void	error(int e);
+void	error_with_arg(int e, const char *str);
 
 //untils
 void	cleanup(t_pipex *p);
