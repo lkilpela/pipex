c7ad6ca6b7db336b624b9ac3d86dba847d4c3335
lumikil <kilpelainenlumi@gmail.com>
2024-04-04 12:18:46 +0300

    Update launch.json and pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:47 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/04 11:00:09 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/04 11:37:42 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ static int	is_directory(char *cmd)
 {
 	int	fd;
 
-	if (access(cmd, F_OK) != 0)
-		error(ERR_FILE);
+	//if (access(cmd, F_OK) != 0)
+		//error(ERR_FILE);
 	fd = open(cmd, O_DIRECTORY);
 	if (fd != -1)
 	{
@@ -33,9 +33,14 @@ static void	parse_and_validate_command(t_pipex *p, t_command *c, char *cmd)
 
 	init_tokenize(&t);
 	c->args = split_command(&t, cmd);
+	for(int i = 0; c->args; i++)
+	{
+		printf("c->args: %s\n", c->args[i]);
+	}
 	if (c->args == NULL || c->args[0] == NULL)
 		error(ERR_CMD);
 	c->path = find_command(p, c->args[0]);
+	printf("c->path: %s\n", c->path);
 	if (!c->path)
 		error_with_arg(ERR_CMD, c->args[0]);
 	if (access(c->path, X_OK) != 0)
@@ -50,7 +55,7 @@ static void	validate_arguments(t_pipex *p)
 		error(ERR_FILE);
 	if (access(p->argv[1], F_OK) != 0)
 		error(ERR_FILE);
-	if (!is_directory(p->argv[2]) || !is_directory(p->argv[3]))
+	if (is_directory(p->argv[2]) || is_directory(p->argv[3]))
 		error(ERR_DIR);
 	parse_and_validate_command(p, &p->cmds[0], p->argv[2]);
 	parse_and_validate_command(p, &p->cmds[1], p->argv[3]);
