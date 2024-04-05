3d83f9c72c9da75ce148f614fd997101556db5eb
lumikil <kilpelainenlumi@gmail.com>
2024-04-05 12:20:36 +0300

    Update execute_commands function in wait.c

--- src/wait.c
+++ src/wait.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/05 10:19:37 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/05 12:20:29 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,27 @@ void	child_status(pid_t pid, int status)
 			(unsigned int)pid, WTERMSIG(status));
 }
 
-int	wait_children(t_pipex *p)
+int	execute_commands(t_pipex *p)
 {
 	pid_t	pid;
 	int		i;
-	int		status;
+	int		exec_status;
+	int		wait_status;
 
 	i = 0;
-	status = execute_first_command(p);
-	if (status != 0)
-		return (status);
-	status = execute_second_command(p);
-	if (status != 0)
-		return (status);
+	exec_status = execute_first_command(p);
+	if (exec_status != 0)
+		return (exec_status);
+	exec_status = execute_second_command(p);
+	if (exec_status != 0)
+		return (exec_status);
 	while (i < 2)
 	{
-		pid = waitpid(p->pids[i], &status, 0);
+		pid = waitpid(p->pids[i], &wait_status, 0);
 		if (pid == -1)
 			error(ERR_WAITPID);
-		else if (status == ERR_PIPE || status == ERR_FORK)
-			child_status(pid, status);
+		else if (exec_status == ERR_PIPE || exec_status == ERR_FORK)
+			child_status(pid, wait_status);
 		i++;
 	}
 	return (0);
