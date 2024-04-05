47a539b17408dbb28e7957f79b150a2710f2e986
lumikil <kilpelainenlumi@gmail.com>
2024-04-05 14:38:50 +0300

    fix norm

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:47 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/05 14:37:53 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:38:39 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ static void	setup_pipe(t_pipex *p)
 	if (pipe(p->pipefd) == -1)
 		error(ERR_PIPE);
 }
+
 //system("leaks pipex");
 int	main(int argc, char **argv, char **envp)
 {
