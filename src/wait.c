78051e1de46b47f3491154e17393b528c53acec3
lumikil <kilpelainenlumi@gmail.com>
2024-04-05 12:44:50 +0300

    Refactor child_status function and add handling for unexpected status

--- src/wait.c
+++ src/wait.c
@@ -6,20 +6,22 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/05 12:37:03 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/05 12:44:43 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	child_status(pid_t pid, int status)
+static void	check_status(pid_t pid, int status)
 {
-	if (WEXITSTATUS(status) != 0)
+	if (WIFEXITED(status) != 0)
 		ft_printf("Child with PID %u exited with status %u.\n",
 			(unsigned int)pid, WEXITSTATUS(status));
 	else if (WIFSIGNALED(status))
 		ft_printf("Child with PID %u was terminated by signal %u\n",
 			(unsigned int)pid, WTERMSIG(status));
+	else
+		ft_printf("Unexpected status %d\n", status);
 }
 
 int	execute_commands(t_pipex *p)
@@ -41,7 +43,7 @@ int	execute_commands(t_pipex *p)
 		pid = waitpid(p->pids[i], &wait_status, 0);
 		if (pid == -1)
 			error(ERR_WAITPID);
-		child_status(pid, wait_status);
+		check_status(pid, wait_status);
 		i++;
 	}
 	return (0);
