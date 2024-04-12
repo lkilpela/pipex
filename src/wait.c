4761cd4fadb5381b650411d177d08ae14a55e772
lumikil <kilpelainenlumi@gmail.com>
2024-04-12 21:24:43 +0300

    Update signal handling in wait.c

--- src/wait.c
+++ src/wait.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/12 21:14:25 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/12 21:24:38 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,46 +23,40 @@
 	else
 		ft_printf("Unexpected status %d\n", status);
 }*/
-static int	signal_exit(t_pipex *p, int status)
+static int	signal(int status)
 {
 	int	signal;
 
-	if (WIFSIGNALED(status)) 
+	if (WIFSIGNALED(status))
 	{
-        signal = WTERMSIG(status);
-        if (signal == SIGINT || signal == SIGQUIT)
-            print_error(SIGQUIT, "SIGQUIT or SIGINT received");
-        else if (signal == SIGPIPE)
-            print_error(SIGPIPE, "SIGPIPE received");
-        else if (signal == SIGCHLD)
-            print_error(SIGCHLD, "SIGCHLD received");
-        else if (signal == SIGSEGV)
-            print_error(SIGSEGV, "SIGSEGV received");
-        else if (signal == SIGBUS)
-            print_error(SIGBUS, "SIGBUS received");
-        else if (signal == SIGKILL)
-            print_error(SIGKILL, "SIGKILL received");
-        else if (signal == SIGABRT)
-            print_error(SIGABRT, "SIGABRT received");
-        else if (signal == SIGTERM)
-            print_error(SIGTERM, "SIGTERM received");
-        else
-            print_error(LAST_ERROR, "Unknown signal received");
-        return ERR_SIG + signal;
-    } else 
-	{
-        ft_printf("Unexpected status %d\n", status);
-        return (status);
-    }
+		signal = WTERMSIG(status);
+		if (signal == SIGINT || signal == SIGQUIT)
+			print_error(SIGQUIT, "SIGQUIT or SIGINT received");
+		else if (signal == SIGPIPE)
+			print_error(SIGPIPE, "SIGPIPE received");
+		else if (signal == SIGCHLD)
+			print_error(SIGCHLD, "SIGCHLD received");
+		else if (signal == SIGSEGV)
+			print_error(SIGSEGV, "SIGSEGV received");
+		else if (signal == SIGBUS)
+			print_error(SIGBUS, "SIGBUS received");
+		else if (signal == SIGKILL)
+			print_error(SIGKILL, "SIGKILL received");
+		else if (signal == SIGABRT)
+			print_error(SIGABRT, "SIGABRT received");
+		else if (signal == SIGTERM)
+			print_error(SIGTERM, "SIGTERM received");
+		return (ERR_SIG + signal);
+	}
 }
 
 static void	check_status(pid_t pid, int status)
 {
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
-	
+	else
+		return (signal_exit(status));
 }
-
 
 int	execute_commands(t_pipex *p)
 {
