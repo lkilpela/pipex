13c17e7fa1cf3903d4f6e28f05e4b5620fd28a96
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-04-12 12:20:05 +0000

    Remove lsof command and unnecessary sleep calls in pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 11:49:47 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/12 12:16:43 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/12 12:20:01 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,13 @@ int	main(int argc, char **argv, char **envp)
 	init_pipex(argc, argv, envp, &p);
 	validate_arguments(&p);
 	setup_pipe(&p);
-	
-	// Add a 1-second delay
-    sleep(1);
-
-    // Run lsof to check open files
-    sprintf(lsof_cmd, "lsof -p %d", getpid());
-    system(lsof_cmd);
-	
 	status = execute_commands(&p);
 	if (status != 0)
 	{
 		cleanup(&p);
 		return (status);
 	}
-
-	// Add a 1-second delay
-    sleep(1);
-
-    // Run lsof to check open files
-    sprintf(lsof_cmd, "lsof -p %d", getpid());
-    system(lsof_cmd);
-
 	close_all_fds(&p);
 	cleanup(&p);
-	    // Run lsof to check open files
-    sprintf(lsof_cmd, "lsof -p %d", getpid());
-    system(lsof_cmd);
 	return (0);
 }
