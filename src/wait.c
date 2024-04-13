4c95002e24d28f644fd8c27db4bae6363c8c0de6
lumikil <kilpelainenlumi@gmail.com>
2024-04-13 11:44:21 +0300

    Refactor signal handling in wait.c

--- src/wait.c
+++ src/wait.c
@@ -6,24 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:41:17 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/13 11:42:46 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/13 11:44:20 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <signal.h>
 
-/*static void	check_status(pid_t pid, int status)
-{
-	if (WIFEXITED(status))
-		ft_printf("Child with PID %u exited with status %u.\n",
-			(unsigned int)pid, WEXITSTATUS(status));
-	else if (WIFSIGNALED(status))
-		ft_printf("Child with PID %u was terminated by signal %u\n",
-			(unsigned int)pid, WTERMSIG(status));
-	else
-		ft_printf("Unexpected status %d\n", status);
-}*/
 static int	handle_signal(t_pipex *p)
 {
 	if (p->wstatus == SIGINT || p->wstatus == SIGQUIT)
@@ -54,12 +43,6 @@ static int	check_status(t_pipex *p)
 	else
 		return (WEXITSTATUS(p->wstatus));		
 }
-/*void sigterm_handler(int signal) {
-	int ret;
-    printf("Received signal %d\n", signal);
-	ret = handle_signal(signal);
-	printf("handle signal returing %d\n", ret);
-}*/
 
 int	execute_commands(t_pipex *p)
 {
@@ -74,11 +57,8 @@ int	execute_commands(t_pipex *p)
 	exec_status = execute_second_command(p);
 	if (exec_status != 0)
 		return (exec_status);
-	//signal(SIGTERM, sigterm_handler);
-	//printf("PArent process PID: %d\n", getpid());
 	while (i < 2)
 	{
-		//sleep(15);
 		if (kill(p->pids[i], SIGTERM) == -1) {
 			perror("kill");
 			return 1;
